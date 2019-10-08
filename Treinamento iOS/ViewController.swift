1981b83aa38de4920a0de37344f9f5115c627da7
Pedro Moura <pedro.moura@socialbank.com.br>
2019-10-08 10:16:53 -0300

    Initial Commit

---
+++ Treinamento iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Treinamento iOS
+//
+//  Created by Pedro Henrique de Moura on 08/10/19.
+//  Copyright © 2019 Social Bank. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Treinamento iOSTests/Treinamento_iOSTests.swift
@@ -0,0 +1,34 @@
+//
+//  Treinamento_iOSTests.swift
+//  Treinamento iOSTests
+//
+//  Created by Pedro Henrique de Moura on 08/10/19.
+//  Copyright © 2019 Social Bank. All rights reserved.
+//
+
+import XCTest
+@testable import Treinamento_iOS
+
+class Treinamento_iOSTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
