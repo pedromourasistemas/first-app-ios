2b48e0d41677eb50c5005d0d9c7eea06c5172a0c
Pedro Moura <pedro.moura@socialbank.com.br>
2019-10-21 18:11:47 -0300

    Adjusts

--- Treinamento iOS/TableViewController.swift
+++ Treinamento iOS/TableViewController.swift
@@ -8,10 +8,22 @@
 
 import UIKit
 
-class TableViewController: UITableViewController {
+class TableViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    //Data Source
     /*var transactionsItems = [
         TransactionItem(id: 1, type: "SUPERMERCADO", title: "Armazém João das Couves", subTitle: "", amount: 1500.00, hour: "09:00AM"),
         TransactionItem(id: 2, type: "TRANSFERÊNCIA", title: "Aluguel Casa", subTitle: "", amount: 2500.00, hour: "07:00PM"),
         TransactionItem(id: 3, type: "ASSINATURAS", title: "Spotify", subTitle: "", amount: 17.90, hour: "4:00PM")
     ]*/
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
 }
